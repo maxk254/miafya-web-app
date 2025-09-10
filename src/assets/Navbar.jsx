fc8fd8f1662724e7453d089a3258723112ebe4cb
max254 <maxkamau77@gmail.com>
2025-09-10 13:23:59 +0300

    navbar-file

--- src/assets/Navbar.jsx
+++ src/assets/Navbar.jsx
@@ -1,51 +1,83 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import miafyalogo from "../assets/miafyalogo.png";
-import { NavLink, useNavigate } from 'react-router-dom';
-
+import { NavLink, useNavigate } from "react-router-dom";
+import logged from "../assets/logged.png";
+import dropdown from "../assets/dropdown.png";
 const Navbar = () => {
-
   const navigate = useNavigate();
 
-    const [showMenu, setShowMenu] =useState(false)
-    const [token, setToken] = useState(false)
+  const [showMenu, setShowMenu] = useState(false);
+  const [token, setToken] = useState(true);
 
   return (
-    <div className="navbar">
+    <div className="flex items-center justify-between text-lg  py- mb-5 border-b border-b-gray-400">
       <img
         onClick={() => navigate("/")}
         src={miafyalogo}
         alt="miafya"
-        width="100px"
-        className="img"
+        width={90}
+        className="cursor-pointer"
       />
-      <ul>
+      <ul className="hidden md:flex items-start gap-5 font-medium bg-white">
         <NavLink to="/">
-          <li>HOME</li>
+          <li className="py-1 hover:bg-teal-200 rounded-full px-3">HOME</li>
         </NavLink>
         <NavLink to="/healthprofesionals">
-          <li>HEALTH PROFESSIONALS</li>
+          <li className="py-1 hover:bg-teal-200 rounded-full px-3">
+            HEALTH PROFESSIONALS
+          </li>
         </NavLink>
         <NavLink to="/healthfacilities">
-          <li>HEALTH FACILITIES</li>
+          <li className="py-1 hover:bg-teal-200 rounded-full px-3">
+            HEALTH FACILITIES
+          </li>
         </NavLink>
         <NavLink to="/contact">
-          <li>CONTACTS</li>
+          <li className="py-1 hover:bg-teal-200 rounded-full px-3">CONTACTS</li>
         </NavLink>
         <NavLink to="/login">
-          <li>LOGIN</li>
+          <li className="py-1 hover:bg-teal-200 rounded-full px-3">LOGIN</li>
         </NavLink>
       </ul>
       <div>
         {token ? (
-          <div>
-            <img src="" alt="" />
+          <div className="flex items-center gap-2 cursor-pointer group relative">
+            <img className="w-8 rounded-full" src={logged} alt="" />
+            <img className="w-7" src={dropdown} alt="" />
+            <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
+              <div className="min-w-50 bg-stone-100 rounded flex flex-col gap-4 p-4">
+                <p
+                  onClick={() => navigate("/Myprofile")}
+                  className="hover:text-black cursor-pointer"
+                >
+                  My profile
+                </p>
+                <p
+                  onClick={() => navigate("/Myappointment")}
+                  className="hover:text-black cursor-pointer"
+                >
+                  My Appointment
+                </p>
+                <p
+                  onClick={() => setToken(false)}
+                  className="hover:text-black cursor-pointer"
+                >
+                  Log-out
+                </p>
+              </div>
+            </div>
           </div>
         ) : (
-          <button onClick={() => navigate("/login")}>Get started</button>
+          <button
+            className="bg-primary text-white px-3 py-3 rounded-full font-light hidden md:block cursor-pointer"
+            onClick={() => navigate("/login")}
+          >
+            Get started
+          </button>
         )}
       </div>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;

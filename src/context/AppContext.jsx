80fc176f1a3572c4e98bf2709ad197107beccf3e
max254 <maxkamau77@gmail.com>
2025-10-15 07:24:26 +0300

    Appcontext

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,11 +1,11 @@
 import { createContext } from "react";
 import { doctors } from "../assets/assets";
-
+import axios from "axios";
 export const AppContext = createContext()
 
 const AppContextprovider = (props) => {
 
-
+const backendUrl = import.meta.env.VITE_BACKEND_URL
 
 const value ={
   doctors

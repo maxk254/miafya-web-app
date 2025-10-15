b4daad12d1bd8828191ed0a850d2faf55ac90b87
max254 <maxkamau77@gmail.com>
2025-10-15 07:23:21 +0300

    navbar-file

--- src/assets/Navbar.jsx
+++ src/assets/Navbar.jsx
@@ -3,6 +3,7 @@ import miafyalogo from "../assets/miafyalogo.png";
 import { NavLink, useNavigate } from "react-router-dom";
 import logged from "../assets/logged.png";
 import dropdown from "../assets/dropdown.png";
+import menu from "../assets/menu.png";
 const Navbar = () => {
   const navigate = useNavigate();
 
@@ -48,20 +49,17 @@ const Navbar = () => {
               <div className="min-w-50 bg-stone-100 rounded flex flex-col gap-4 p-4">
                 <p
                   onClick={() => navigate("/Myprofile")}
-                  className="hover:text-black cursor-pointer"
-                >
+                  className="hover:text-black cursor-pointer">
                   My profile
                 </p>
                 <p
                   onClick={() => navigate("/Myappointment")}
-                  className="hover:text-black cursor-pointer"
-                >
+                  className="hover:text-black cursor-pointer">
                   My Appointment
                 </p>
                 <p
                   onClick={() => setToken(false)}
-                  className="hover:text-black cursor-pointer"
-                >
+                  className="hover:text-black cursor-pointer">
                   Log-out
                 </p>
               </div>
@@ -70,11 +68,25 @@ const Navbar = () => {
         ) : (
           <button
             className="bg-primary text-white px-3 py-3 rounded-full font-light hidden md:block cursor-pointer"
-            onClick={() => navigate("/login")}
-          >
+            onClick={() => navigate("/login")}>
             Get started
           </button>
         )}
+        <img onClick={()=>setShowMenu(true)} className="w-10 md:hidden cursor-pointer" src={menu} alt="menu-icon"/>
+        {/* mobile menu */}
+        <div className={` ${showMenu ? 'fixed w-full' :'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+          <div className="flex items-center justify-between px-5 py-6">
+            <img onClick={()=>navigate("/")} className="cursor pointer w-36" src={miafyalogo} alt="logo-icon"  />
+            <img className="cursor-pointer w-7" onClick={()=>setShowMenu(false)} src="" alt="cross-icon" />
+          </div>
+          <ul className="flex flex-col items-start gap-2 mt-5 px-5 text-lg font-medium">
+            <NavLink className="px-4 py-2 rounded-full inline-block hover:bg-teal-300" onClick={()=>setShowMenu(false)} to='/'>HOME</NavLink>
+            <NavLink className='px-4 py-2 rounded-full inline-block hover:bg-teal-300' onClick={()=>setShowMenu(false)} to='healthprofesionals'>HEALTH PROFESSIONALS</NavLink>
+            <NavLink className='px-4 py-2 rounded-full inline-block hover:bg-teal-300' onClick={()=>setShowMenu(false)} to='healthfacilities'>HEALTH FACILITIES</NavLink>
+            <NavLink className='px-4 py-2 rounded-full inline-block hover:bg-teal-300' onClick={()=>setShowMenu(false)} to='contact'>CONTACT</NavLink>
+            <NavLink className='px-4 py-2 rounded-full inline-block hover:bg-teal-300' onClick={()=>setShowMenu(false)} to='login'>LOGIN</NavLink>
+          </ul>
+        </div>
       </div>
     </div>
   );

36ca0818e9ceda19b5da2721aabdcb861ff961d1
max254 <maxkamau77@gmail.com>
2025-10-28 22:22:19 +0300

    facilitybutton

--- src/assets/Components/FacilityButton.jsx
+++ src/assets/Components/FacilityButton.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const FacilityButton = () => {
+    const navigate = useNavigate();
   return (
     <div>
-      <a
-        className="flex items-center gap-2 bg-teal-400 px-8 py-3 rounded-full tex-gray-900 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300"
-        href="#">
+      <button
+        onClick={() => navigate(`/facilityReg`)}
+        className="flex items-center gap-2 bg-teal-400 px-8 py-3 rounded-full
+        tex-gray-900 text-sm m-auto md:m-0 hover:scale-105 transition-all
+        duration-300">
+          
         Register you facility to join our plaform
-      </a>
+      </button>
     </div>
   );
 }

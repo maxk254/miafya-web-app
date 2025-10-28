c4ce4590c5dd17c49e730bf579db178a5a12cc7c
max254 <maxkamau77@gmail.com>
2025-10-28 22:24:55 +0300

    proffesionalbtn-file

--- src/assets/Components/Proffesionalbtn.jsx
+++ src/assets/Components/Proffesionalbtn.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Proffesionalbtn = () => {
+
+  const navigate = useNavigate();
+
   return (
     <div>
-      <a
-        className="flex items-center gap-2 bg-teal-400 px-8 py-3 rounded-full tex-gray-900 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300"
-        href="#"
-      >
+      <button
+        onClick={() => navigate('/docReg')}
+        className="flex items-center gap-2 bg-teal-400 px-8 py-3 rounded-full
+        tex-gray-900 text-sm m-auto md:m-0 hover:scale-105 transition-all
+        duration-300">
         For Healthcare Professionals
-      </a>
+      </button>
     </div>
   );
 }

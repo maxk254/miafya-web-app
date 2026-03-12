b09537271be1268a2159d083655dd6245e95ab3e
max254 <maxkamau77@gmail.com>
2026-03-13 02:09:10 +0300

    toast alert across entire app

---
+++ src/context/NotificationContext.jsx
@@ -0,0 +1,11 @@
+// toast alerts across entire app
+
+import React from 'react'
+
+const NotificationContext = () => {
+  return (
+    <div>NotificationContext</div>
+  )
+}
+
+export default NotificationContext

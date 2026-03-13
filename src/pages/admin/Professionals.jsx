09a3e8c0914d2e4d76af2981afdbd4da5e963100
max254 <maxkamau77@gmail.com>
2026-03-14 00:02:19 +0300

    function to get all professional + verification status

---
+++ src/pages/admin/Professionals.jsx
@@ -0,0 +1,10 @@
+// all professionals + verification status
+import React from 'react'
+
+const Professionals = () => {
+  return (
+    <div>Professionals</div>
+  )
+}
+
+export default Professionals

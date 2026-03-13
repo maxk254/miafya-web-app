fefb1cb4f45b8e4f293afd33e5ef4ed8a4e8ecf5
max254 <maxkamau77@gmail.com>
2026-03-13 03:09:29 +0300

    page to pick and book and pay for appoitment

---
+++ src/pages/patients/BookAppointment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+// search → pick doctor → pick slot → pay
+const BookAppointment = () => {
+  return (
+    <div>BookAppointment</div>
+  )
+}
+
+export default BookAppointment

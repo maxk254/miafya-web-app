f3bf75b5915105f51f7b9b0c279e4c03cc8f3b07
max254 <maxkamau77@gmail.com>
2026-03-13 23:54:41 +0300

    function to view submited docs and approve or reject approval

---
+++ src/pages/admin/ProfessionalDetail.jsx
@@ -0,0 +1,10 @@
+// view docs, approve/reject verification
+import React from 'react'
+
+const ProfessionalDetail = () => {
+  return (
+    <div>ProfessionalDetail</div>
+  )
+}
+
+export default ProfessionalDetail

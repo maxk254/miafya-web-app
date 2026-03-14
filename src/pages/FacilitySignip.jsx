a24854d196644f4ab392fd96b055e2794ffc02f4
max254 <maxkamau77@gmail.com>
2026-03-14 11:41:50 +0300

    fixed file strucures

--- src/pages/FacilitySignip.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FacilitySignip = () => {
-  return (
-    <div>FacilitySignip</div>
-  )
-}
-
-export default FacilitySignip
--- src/pages/FacilitySignup.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FacilitySignup = () => {
-  return (
-    <div>FacilitySignup</div>
-  )
-}
-
-export default FacilitySignup

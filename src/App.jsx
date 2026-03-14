37fde850c6924e352d28533ebe23f99de4760391
max254 <maxkamau77@gmail.com>
2026-03-14 11:33:33 +0300

    to fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,41 @@
 import React from "react";
 import "./App.css";
 // import Navbar from "./assets/Navbar";
-import Home from "./pages/Home";
-import Healthprofessionals from "./pages/Healthprofessionals";
+import Home from "./pages/public/Home";
+import Healthprofessionals from "./pages/public/Healthprofessionals";
 import {Route,createBrowserRouter, createRoutesFromElements, RouterProvider} from "react-router-dom";
-import Login from "./pages/Login";
-import Healthfacilities from "./pages/Healthfacilities";
-import RootLayout from "./layout/RootLayout";
-import Contact from "./pages/Contact";
-import Myprofile from "./pages/Myprofile";
-import Myappointments from "./pages/Myappointments";
-import About from "./pages/About";
-import Appointment from "./pages/Appointment";
-import FacilityReg from "./pages/FacilityReg";
-import DocReg from "./pages/DocReg";
+import AuthOnboard from "./pages/auth/Signup";
+// import Healthfacilities from "./pages/Healthfacilities";
+import RootLayout from "./Components/layout/RootLayout";
+import Contact from "./pages/public/Contact";
+import Myprofile from "./pages/patients/Myprofile";
+import Myappointments from "./pages/patients/Myappointments";
+import About from "./pages/public/About";
+import Login from "./pages/auth/Login";
+// import Appointment from "./pages/Appointment"; TO REPLACE THIS WITH MYAPPINTMENT FILE ON THE PATIENT FOLDER
+
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />}>
         <Route index element={<Home />} />
         <Route path="healthprofesionals" element={<Healthprofessionals />} />
-        <Route path="healthfacilities" element={<Healthfacilities />} />
-        {/* <Route path="login" element={<Login />} /> */}
+        {/* <Route path="healthfacilities" element={<Healthfacilities />} /> */}
+        <Route path="authonboard" element={<AuthOnboard />} />
+        <Route path="login" element={<Login />} />
         <Route path="contact" element={<Contact />}></Route>
         <Route path="Myprofile" element={<Myprofile />} />
         <Route path="Myappointment" element={<Myappointments />} />
-        <Route path="appointment/:docId" element={<Appointment />} />
+        {/* <Route path="appointment/:docId" element={<Appointment />} />   {TO REPLACE THIS WITH THE MYAPPOINTMENT FILE ON THE PATIENT FOLDER} */}
         <Route path="about" element={<About />} />
         {/* <Route path="facilityReg" element={<FacilityReg />} />
         <Route path="docReg" element={<DocReg />} /> */}
-        <Route path="healthprofesionals/:speciality"element={<Healthprofessionals />}/>
-      </Route>
-    )
+        <Route
+          path="healthprofesionals/:speciality"
+          element={<Healthprofessionals />}
+        />
+      </Route>,
+    ),
   );
 
   return (

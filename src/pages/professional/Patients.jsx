8578a12846f3294a8915f27339a2a94e95034906
max254 <maxkamau77@gmail.com>
2026-03-14 10:16:47 +0300

    function to see all patiens who booked for appointments

---
+++ src/pages/professional/Patients.jsx
@@ -0,0 +1,10 @@
+// all patients info who jave booked with the professional
+import React from 'react'
+
+const Patients = () => {
+  return (
+    <div>Patients</div>
+  )
+}
+
+export default Patients

bc07b88b40d2e67b3a766ccd236408d0d8a72c7e
max254 <maxkamau77@gmail.com>
2026-03-14 10:17:26 +0300

    reusable ui

---
+++ src/pages/professional/ProfessionalLayout.jsx
@@ -0,0 +1,11 @@
+// sidebar + topbar shell
+
+import React from 'react'
+
+const ProfessionalLayout = () => {
+  return (
+    <div>ProfessionalLayout</div>
+  )
+}
+
+export default ProfessionalLayout
